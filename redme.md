face935e34974d08ca4d5da19fdd291f99e0bdb9
mattmoana <matt.morozov@oc.eu>
2024-11-14 20:23:16 +0200

    Initial commit

---
+++ redme.md
@@ -0,0 +1,9 @@
+1. Check that place order button is enabled when correct data is filled and pop up message is OK
+2. When correct data is removed the button is disabled
+
+
+
+button = data-testid="submit-order"
+username = data-testid="username"
+email = data-testid="email"
+ok pop up = #popup-message
---
+++ tests/example.spec.ts
@@ -0,0 +1,37 @@
+import { test, expect } from '@playwright/test';
+
+test.beforeEach(async ({ page }) => {
+  const path = require('path');
+  const filePath = `file://${path.resolve('order.html')}`;
+  await page.goto(filePath);
+})
+
+test('Check that place order button is enabled when correct data is filled and pop up message is OK', async ({ page }) => {
+  const orderButton = page.getByTestId("submit-order")
+  const usernameInput = page.getByTestId("username")
+  const emailInput = page.getByTestId("email")
+  const okPopUp = page.locator("#popup-message")
+
+  await expect(orderButton).toBeDisabled()
+  await usernameInput.fill("testUser")
+  await emailInput.fill("testUser@test.com")
+  await expect(orderButton).toBeEnabled()
+
+  await orderButton.click()
+  await expect(okPopUp).toBeVisible()
+});
+
+test('When correct data is removed the button is disabled', async ({ page }) => {
+  const orderButton = page.getByTestId("submit-order")
+  const usernameInput = page.getByTestId("username")
+  const emailInput = page.getByTestId("email")
+  const okPopUp = page.locator("#popup-message")
+
+  await usernameInput.fill("testUser")
+  await emailInput.fill("testUser@test.com")
+  await expect(orderButton).toBeEnabled()
+
+  await usernameInput.fill("")
+  await emailInput.fill("")
+  await expect(orderButton).toBeDisabled()
+});
